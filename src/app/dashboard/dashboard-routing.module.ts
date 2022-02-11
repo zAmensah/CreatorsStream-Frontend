c304470e3c466c66eb4da86a66554d7c60ed1865
zAmensah <zamensah840@gmail.com>
2022-02-11 09:45:53 +0000

    single channel route

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AddVideoComponent } from './components/add-video/add-video.component';
 import { ChannelsComponent } from './components/channels/channels.component';
 import { CreateChannelComponent } from './components/channels/create-channel/create-channel.component';
+import { SingleChannelComponent } from './components/channels/single-channel/single-channel.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { SettingsComponent } from './components/settings/settings.component';
 
@@ -10,7 +11,7 @@ const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'add-video', component: AddVideoComponent },
   { path: 'channels', component: ChannelsComponent },
-  { path: 'channels/single/:id', component: ChannelsComponent },
+  { path: 'channels/single/:id', component: SingleChannelComponent },
   { path: 'channel/add', component: CreateChannelComponent },
   { path: 'settings', component: SettingsComponent },
 ];
