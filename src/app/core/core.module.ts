c3e8f4d12388978c54aac9704a14ed55bb57b76d
zAmensah <zamensah840@gmail.com>
2022-02-15 10:50:16 +0000

    navbar changes

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,9 +5,11 @@ import { CoreRoutingModule } from './core-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { ViewPageComponent } from './components/view-page/view-page.component';
 import { SharedModule } from '../shared/modules/shared/shared.module';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { FooterComponent } from './layouts/footer/footer.component';
 
 @NgModule({
-  declarations: [HomeComponent, ViewPageComponent],
+  declarations: [HomeComponent, ViewPageComponent, NavbarComponent, FooterComponent],
   imports: [CommonModule, CoreRoutingModule, SharedModule],
 })
 export class CoreModule {}
---
+++ src/app/core/layouts/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/services/auth.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+})
+export class NavbarComponent implements OnInit {
+  constructor(public authService: AuthService) {}
+
+  logout() {
+    this.authService.logout();
+    window.location.reload();
+  }
+
+  ngOnInit(): void {}
+}
