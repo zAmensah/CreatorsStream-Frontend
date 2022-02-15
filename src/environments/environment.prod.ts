19e5a3179da21a07df37bd911a15eaed0a5ff9d9
zAmensah <zamensah840@gmail.com>
2022-02-15 13:05:38 +0000

    clean up #3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  basedUrl: 'https://creators-stream.herokuapp.com/api',
+  firebaseConfig: {
+    apiKey: 'AIzaSyBJFAVvWxgc8gjx1qWyVmLji-esqpWvzrg',
+    authDomain: 'creators-stream.firebaseapp.com',
+    projectId: 'creators-stream',
+    storageBucket: 'creators-stream.appspot.com',
+    messagingSenderId: '402363483677',
+    appId: '1:402363483677:web:698b2a5e68d933e5d368f4',
+    measurementId: 'G-Z1VQTEPQW6',
+  },
 };
